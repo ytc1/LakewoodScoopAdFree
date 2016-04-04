c62f5c03386523617e732b162cf1a569fc65a46d
Yechiel Cohen <ytc92@yahoo.com>
2016-04-04 10:20:59 -0400

    First Commit

---
+++ LakewoodScoopAdFree/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LakewoodScoopAdFree
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Image { get; set; }
+    }
+}
